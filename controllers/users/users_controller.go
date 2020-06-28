6a34361903411a624b750c24b04e6acfb84318d5
moatazsalemVF <moutazsalem@gmail.com>
2020-06-28 13:30:38 +0200

    basic

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,24 @@
 package users
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/moatazsalemVF/bookstore_user-api/domain/users"
 )
 
 //CreateUser is a func to create users
 func CreateUser(c *gin.Context) {
+	user := users.User{}
+	fmt.Println(user)
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	fmt.Println(string(bytes))
+	if err != nil {
+		//TODO: Handle Error here
+		return
+	}
 	c.String(http.StatusNotImplemented, "Implement me!")
 }
 
--- domain/users/users.go
+++ domain/users/users.go
@@ -1,1 +1,10 @@
 package users
+
+//User is the main domain
+type User struct {
+	id          int64
+	firstName   string
+	lastName    string
+	email       string
+	dateCreated string
+}
